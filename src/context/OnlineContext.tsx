c8f671ad633f90caa8fe7dedd4d317ede946a8eb
ariel <ariel10player@gmail.com>
2025-12-21 01:47:48 -0300

    feat:  migrar datos de LS a DB

--- src/context/OnlineContext.tsx
+++ src/context/OnlineContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState, type ReactNode } from "react";
+import { useSell } from "./useSell";
 
 
 interface OnlineContextInterface {
@@ -9,6 +10,10 @@ export const OnlineContext = createContext<OnlineContextInterface | undefined>(u
 
 export const OnlineProvider = ({ children }: { children: ReactNode }) => {
     const [online, SetOnline] = useState<boolean>(navigator.onLine)
+    const { ventas } = useSell()
+
+
+
 
     useEffect(() => {
         const handleChange = () => SetOnline(navigator.onLine)
@@ -19,6 +24,37 @@ export const OnlineProvider = ({ children }: { children: ReactNode }) => {
         window.removeEventListener("offline", handleChange)
 
     }, [])
+
+
+
+
+    useEffect(() => {
+        if (online && ventas != undefined) {
+            ventas.forEach(async (v) => {
+                if (v.onDb == false) {
+                    try {
+                        v.onDb = true
+                        const res = await fetch(`https://app-helados-backend.onrender.com/sell`, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify(v)
+                        })
+                        if (res.ok) {
+                            console.log("Venta agregada a la base de datos exitosamente", v)
+                        } else {
+                            console.log("Error al enviar la venta a la base de datos", "venta en cuestión:", v)
+                        }
+                    } catch (e) {
+                        console.log("Error al enviar la venta a la base de datos", "venta en cuestión:", v, "Error:", e)
+                    }
+                }
+
+            })
+        }
+    }, [online, ventas])
+
 
 
     return (<OnlineContext.Provider value={{
