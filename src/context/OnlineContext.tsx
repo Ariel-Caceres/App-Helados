22d3448c9cedbb6527fad09891508b56b6ead3e0
ariel <ariel10player@gmail.com>
2025-12-29 04:31:33 -0300

    manejo de asinronia ahora en Online context

--- src/context/OnlineContext.tsx
+++ src/context/OnlineContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState, type ReactNode } from "react";
 import { useSell } from "./useSell";
-
+import type { Venta } from "./SellContext";
 
 interface OnlineContextInterface {
     online: boolean
@@ -9,14 +9,14 @@ interface OnlineContextInterface {
 export const OnlineContext = createContext<OnlineContextInterface | undefined>(undefined);
 
 export const OnlineProvider = ({ children }: { children: ReactNode }) => {
-    const [online, SetOnline] = useState<boolean>(navigator.onLine)
+
     const { ventas } = useSell()
 
-
+    const [online, setOnline] = useState<boolean>(navigator.onLine)
 
 
     useEffect(() => {
-        const handleChange = () => SetOnline(navigator.onLine)
+        const handleChange = () => setOnline(navigator.onLine)
         window.addEventListener("online", handleChange)
         window.addEventListener("offline", handleChange)
 
@@ -26,34 +26,71 @@ export const OnlineProvider = ({ children }: { children: ReactNode }) => {
     }, [])
 
 
-
-
     useEffect(() => {
         if (online && ventas != undefined) {
             ventas.forEach(async (v) => {
-                if (v.onDb == false) {
+                if (v.status == "pending-create") {
+                    v.status = "synced"
                     try {
-                        v.onDb = true
-                        const res = await fetch(`https://app-helados-backend.onrender.com/sell`, {
+                        const res = await fetch(`${import.meta.env.VITE_API_URL}/sell`, {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
                             },
                             body: JSON.stringify(v)
+
                         })
-                        if (res.ok) {
-                            console.log("Venta agregada a la base de datos exitosamente", v)
-                        } else {
+                        if (!res.ok) {
                             console.log("Error al enviar la venta a la base de datos", "venta en cuestión:", v)
+                        } else {
+                            console.log("Venta agregada a la base de datos exitosamente", v)
                         }
                     } catch (e) {
                         console.log("Error al enviar la venta a la base de datos", "venta en cuestión:", v, "Error:", e)
                     }
                 }
 
+                if (v.status == "pending-update") {
+                    v.status = "synced"
+                    try {
+                        const res = await fetch(`${import.meta.env.VITE_API_URL}/edit/${v.id}`, {
+                            method: "PUT",
+                            headers: {
+                                "Content-type": "application/json"
+                            },
+                            body: JSON.stringify(v)
+                        })
+                        if (res.ok) {
+                            console.log("Venta editada con éxito")
+                        }
+                    } catch (e) { console.log("Venta editada con error", e) }
+                }
+
+                if (v.status == "pending-delete") {
+                    try {
+                        const res = await fetch(`${import.meta.env.VITE_API_URL}/delete/${v.id}`, {
+                            method: "DELETE"
+                        })
+                        if (!res.ok) {
+                            console.log("error al borrar venta", v.id)
+                        } else {
+                            console.log("venta eliminada con éxito")
+                            const ventasBorradas: Venta[] = ventas.filter(ventaaborrar => (ventaaborrar.id !== v.id))
+                            localStorage.setItem("ventas", JSON.stringify(ventasBorradas))
+                        }
+
+                    } catch (e) {
+                        console.log("error al borrar venta", e)
+                    }
+                }
             })
         }
+
     }, [online, ventas])
+
+
+
+
 
 
 
