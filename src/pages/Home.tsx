9ec7c06c31ab5c49d948b79189df48b573325a56
ariel <ariel10player@gmail.com>
2026-01-04 22:23:35 -0300

    tamaño de iconos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,15 +63,9 @@ export const Home = () => {
                 <div className="w-full flex flex-row md:flex-row justify-evenly gap-4 mt-16 px-2">
 
                     {/* Venta hoy */}
-                    <div className="w-1/2 md:w-1/3  
-          bg-[#FFBFA0] 
-          min-h-32 md:h-40 
-          rounded-2xl flex flex-col 
-          border-2 shadow-md
-          p-4
-        ">
+                    <div className="w-1/2 md:w-1/3        bg-[#FFBFA0] min-h-32 md:h-40 rounded-2xl flex flex-col border-2 shadow-md p-4 ">
                         <div className="w-full flex justify-center items-center text-xl md:text-2xl font-medium">
-                            {online ? <i className="fa-solid fa-cash-register text-3xl"></i> : "🧊"}
+                            {online ? <i className="fa-solid fa-cash-register text-2xl"></i> : "🧊"}
                             <span className="">
                                 Ventas hoy:
                             </span>
@@ -91,7 +85,7 @@ export const Home = () => {
           p-4
         ">
                         <div className="w-full flex justify-center items-center text-xl md:text-2xl font-medium">
-                            {online ? <i className="fa-regular fa-money-bill-1 text-3xl"></i> : "💸"}
+                            {online ? <i className="fa-regular fa-money-bill-1 text-2xl"></i> : "💸"}
                             <span className="">
                                 Dinero hoy:
                             </span>
