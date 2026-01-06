67fadffc1c8e7ecf4faf12f31055620840ea15ae
ariel <ariel10player@gmail.com>
2026-01-06 08:18:58 -0300

    borro codigo para ordenar columnas por asc-des

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jq5np85hpk8"
+    "revision": "0.63a3gqg540o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/HeaderTabla.tsx
+++ src/components/HeaderTabla.tsx
@@ -1,43 +1,25 @@
-import { useSell } from "../context/useSell"
 
 
 export const HeaderTabla = () => {
-    const { setOrden, orden } = useSell()
 
-    const ordenHandler = (tipo: string) => {
-        // Definimos el siguiente paso para cada estado
-        const siguienteOrden = {
-            "": `${tipo} Des`,
-            [`${tipo} Des`]: `${tipo} Asd`,
-            [`${tipo} Asd`]: `${tipo} Des`
-        };
-
-        // Buscamos el estado actual en el mapa y actualizamos
-        // Si 'orden' no coincide con el tipo actual, empezamos desde el inicio del ciclo
-        const nuevoEstado = siguienteOrden[orden] || `${tipo} Des`;
-        setOrden(nuevoEstado);
-    };
 
     return (
         <div className="  flex justify-between  " >
-            <div className={`w-1/4 border-2  border-r-0 flex justify-evenly ${orden.includes("fecha") ? "bg-red-200" : ""}`} onClick={() => ordenHandler("fecha")}>
+            <div className={`w-1/4 border-2  border-r-0 flex justify-evenly `} >
                 <span className="pl-2 text-xl font-bold">Fecha</span>
             </div>
-            <div className="w-1/4 border-2 ">
+            <div className="w-1/4 border-2 border-r-0">
                 <span className="pl-2 text-xl font-bold">Producto</span>
             </div>
-            <div className="w-1/4 border-2 border-r-0" onClick={() => ordenHandler("cantidad")}>
+            <div className="w-1/4 border-2 border-r-0" >
                 <span className="pl-1 text-xl font-bold">Cantidad</span>
             </div>
-            <div className="w-1/4 border-2 border-r-0" onClick={() => ordenHandler("precioTotal")}>
+            <div className="w-1/4 border-2 border-r-0" >
                 <span className="pl-2 text-xl font-bold">Precio</span>
             </div>
             <div className="w-1/4 border-2 ">
                 <span className="pl-2 text-xl font-bold">Acciones</span>
             </div>
-
-
-
         </div>
 
     )
