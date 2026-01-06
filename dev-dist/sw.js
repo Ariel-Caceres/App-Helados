dbca06ce48e1bb1c76273437103d81f237d9881f
ariel <ariel10player@gmail.com>
2026-01-06 01:49:30 -0300

    visualización de columna producto

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vgg7g4apvk"
+    "revision": "0.jq5np85hpk8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
