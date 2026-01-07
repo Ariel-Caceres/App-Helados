5044739aabfdd8ae97280ef368978597ee7dd115
ariel <ariel10player@gmail.com>
2026-01-07 01:32:19 -0300

    manejo de filtros por mes, si es 1 que pase a 12 y biseversa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jrkck06njc8"
+    "revision": "0.0lemm768ht"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
