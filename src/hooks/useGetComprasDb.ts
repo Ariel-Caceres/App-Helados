1d14ebe42464499b0421b4fa5303463bad01806b
ariel <ariel10player@gmail.com>
2026-01-14 18:08:23 -0300

    condicion online para traer compras y ventas

--- src/hooks/useGetComprasDb.ts
+++ src/hooks/useGetComprasDb.ts
@@ -1,27 +1,30 @@
 import { useEffect, useState } from "react";
 import type { Compra } from "../types/compra.entity";
-
+import { useOnline } from "./useOnline";
 export const GetComprasDb = (mes: string) => {
     const [comprasDb, setComprasDb] = useState<Compra[] | null>(null)
+    const { online } = useOnline()
 
     useEffect(() => {
-        const traerDataDb = async () => {
-            try {
-                const res = await fetch(`${import.meta.env.VITE_API_URL}/purchases/month/${mes}`)
-                if (!res.ok) {
-                    console.log("Error al traer las compras de la base de datos");
-                } else {
-                    const data = await res.json()
-                    setComprasDb(data)
+        if (online) {
+            const traerDataDb = async () => {
+                try {
+                    const res = await fetch(`${import.meta.env.VITE_API_URL}/purchases/month/${mes}`)
+                    if (!res.ok) {
+                        console.log("Error al traer las compras de la base de datos");
+                    } else {
+                        const data = await res.json()
+                        setComprasDb(data)
+                    }
+
+                } catch (e) {
+                    console.log("Error al traer las compras de la base de datos", e);
+
                 }
-
-            } catch (e) {
-                console.log("Error al traer las compras de la base de datos", e);
-
             }
+            traerDataDb()
         }
-        traerDataDb()
-    }, [mes])
+    }, [mes, online])
 
     return (
         { comprasDb }
--- src/hooks/useGetVentas.ts
+++ src/hooks/useGetVentas.ts
@@ -1,35 +1,38 @@
 import { useEffect, useState } from "react"
 import type { Venta } from "../types/venta.entity"
-
+import { useOnline } from "./useOnline"
 
 export const useGetVentas = (mes: string) => {
     const [cargando, setCargando] = useState<boolean>(false)
     const [error, setError] = useState<string | null>(null)
     const [ventasDb, setVentasDb] = useState<Venta[] | null>(null)
-
+    const { online } = useOnline()
 
     useEffect(() => {
-        const traerDataDb = async () => {
-            setCargando(true)
-            try {
-                const res = await fetch(`${import.meta.env.VITE_API_URL}/sales/month/${mes}`)
-                const data = await res.json()
-                if (!res.ok) {
-                    console.log("Error al traer las ventas de la base de datos");
-                } else {
-                    setVentasDb(data)
-                    console.log("Fetch exitoso a ventas de db");
-                }
+        if (online) {
+            const traerDataDb = async () => {
+                setCargando(true)
+                try {
+                    const res = await fetch(`${import.meta.env.VITE_API_URL}/sales/month/${mes}`)
+                    const data = await res.json()
+                    if (!res.ok) {
+                        console.log("Error al traer las ventas de la base de datos");
+                    } else {
+                        setVentasDb(data)
+                        console.log("Fetch exitoso a ventas de db");
+                    }
 
-            } catch (e) {
-                setError("Error al traer las ventas")
-                console.log("Error al traer las venas de la base de datos", e);
-            } finally {
-                setCargando(false)
+                } catch (e) {
+                    setError("Error al traer las ventas")
+                    console.log("Error al traer las venas de la base de datos", e);
+                } finally {
+                    setCargando(false)
+                }
             }
+            traerDataDb()
         }
-        traerDataDb()
-    }, [mes])
+
+    }, [mes, online])
 
     return (
 
