19de0cb14d381b0fd4eba08a27cc37ddc83b0d80
ariel <ariel10player@gmail.com>
2026-01-14 18:10:27 -0300

    reestructuracion del resumen diario

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.q19hk92btv8"
+    "revision": "0.m4kcprrags"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,15 @@
 import { useNavigate } from "react-router-dom"
 import { Header } from "../components/Header"
-import { useSell } from "../hooks/useSell"
-import { useOnline } from "../hooks/useOnline"
 import { Button } from "../components/Button"
 import { useEffect, useRef, useState } from "react"
 import { MonthResume } from "../components/MonthResume"
-import { useSalesDb } from "../hooks/useSalesDb"
+import { DailyResume } from "../components/DailyResume"
 
 export const Home = () => {
     const navigate = useNavigate()
-    const { ventas, hoy } = useSell()
-    const { online } = useOnline()
-    const { ventasDb } = useSalesDb()
-    const ventasHoy = ventas.filter(v => {
-        return v.fecha.split("-")[2] == hoy.split("-")[2] && v.status != "pending-delete"
-    })
+
     const [mpd, setmpd] = useState(1)
-    const [dineroHoy, setDineroHy] = useState<number>()
-    const [cantHoy, setCantHoy] = useState<number>()
+
     const [animar, setAnimar] = useState<boolean>(false)
     const productos: Record<string, () => string> = {
         1: () => "helado",
@@ -25,18 +17,10 @@ export const Home = () => {
         3: () => "pollo-trozado"
     }
 
-    const calcularTotal = (parametro: string) => {
-        const ventasAMostrar = ventasHoy.filter(v => v.producto ? v.producto == parametro : true)
-        const dineroAMostrar = ventasAMostrar.reduce((acc, v) => v.precioTotal + acc, 0)
-        const cantAMostrar = ventasAMostrar.reduce((acc, v) => v.cantidad + acc, 0)
-        setDineroHy(dineroAMostrar)
-        setCantHoy(cantAMostrar)
-    }
+
     const firstRender = useRef(true)
 
-    useEffect(() => {
-        calcularTotal(productos[mpd]())
-    }, [mpd])
+
 
 
 
@@ -61,57 +45,7 @@ export const Home = () => {
 
             <Header />
 
-            <div className="w-full border flex bg-[#DAF5FF] rounded-2xl   flex-col justify-between relative  overflow-hidden" >
-
-                <div className=" border top-0 left-0 gap-x-10 gap-y-3 right-0 flex flex-wrap w-full  items-center justify-between z-10   bg-amber-200 rounded-2xl px-6 py-4 ">
-
-                    <div className="text-xl whitespace-nowrap md:text-3xl font-medium">
-                        {online ? <i className="fa-solid fa-chart-simple"></i> : "📊"}
-                        <span>
-                            Panel Diario
-                        </span>
-                    </div>
-
-
-                </div>
-
-                <div
-                    className={`
-    w-full flex flex-row md:flex-row justify-evenly gap-4 p-2 px-2
-    transition-all duration-300 ease-in-out
-    filter
-    ${animar ? "-translate-y-12/12" : "translate-y-0"}
-  `}
-                >
-
-                    {/* Venta hoy */}
-                    <div className="w-1/2 md:w-1/3    bg-[#FFBFA0] min-h-20 md:h-25 rounded-2xl flex flex-col border-2 shadow-md p-2 ">
-                        <div className="w-full flex justify-center items-center text-xl md:text-2xl font-medium">
-                            {online ? <i className="fa-solid fa-cash-register text-xl"></i> : "🧊"}
-                            <span className="">
-                                Ventas hoy:
-                            </span>
-                        </div>
-                        <div className="w-full flex justify-center items-center flex-1">
-                            <span className="text-2xl font-bold animate-pulse">{cantHoy}</span>
-                        </div>
-                    </div>
-
-                    {/* Dinero hoy */}
-                    <div className="      w-1/2 md:w-1/3      bg-[#87F6FF]      min-h-20 md:h-25    rounded-2xl flex flex-col      border-2 shadow-md       p-2  ">
-                        <div className="w-full flex justify-center items-center text-xl md:text-2xl font-medium">
-                            {online ? <i className="fa-regular fa-money-bill-1 text-xl"></i> : "💸"}
-                            <span className="">
-                                Dinero hoy:
-                            </span>
-                        </div>
-                        <div className="w-full flex justify-center items-center flex-1">
-                            <span className="text-2xl font-bold animate-pulse">${dineroHoy}</span>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+            <DailyResume animar={animar} producto={productos[mpd]()} />
 
             <MonthResume producto={productos[mpd]()} animar={animar} />
 
