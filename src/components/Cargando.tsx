1c854a6079cd707e7b6fe3d014ded97ffd0806ee
ariel <ariel10player@gmail.com>
2026-01-30 20:40:59 -0300

    feat: componente cargando

---
+++ src/components/Cargando.tsx
@@ -0,0 +1,8 @@
+
+export const Cargando = () => {
+
+    return (
+
+        <div className="flex justify-center items-center border-2 text-xl mt-2 mb-5 p-2 ">Cargando...<span className=" animate-spin">🕐</span></div>
+    )
+}
