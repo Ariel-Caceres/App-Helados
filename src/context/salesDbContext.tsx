7ea3332288ab54f9463054ce252e58ac88229f02
ariel <ariel10player@gmail.com>
2026-02-05 20:14:21 -0300

    fix:; llamo al mes actual en ves de hardcodear 1 como estaba antes

--- src/context/salesDbContext.tsx
+++ src/context/salesDbContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, type ReactNode } from "react";
 import { useGetVentas } from "../hooks/useGetVentas";
 import type { Venta } from "../types/venta.entity";
+import { useSell } from "../hooks/useSell";
 
 interface salesDbInterface {
     ventasDb: Venta[] | null
@@ -10,8 +11,10 @@ interface salesDbInterface {
 
 export const SalesDb = createContext<salesDbInterface | undefined>(undefined)
 export const SalesDbProvider = ({ children }: { children: ReactNode }) => {
-
-    const { ventasDb, cargando, error } = useGetVentas("01")
+    const { hoy } = useSell()
+    const mes = hoy.split("-")[1]
+    console.log(mes)
+    const { ventasDb, cargando, error } = useGetVentas(mes)
 
     return (
         <SalesDb.Provider value={{
